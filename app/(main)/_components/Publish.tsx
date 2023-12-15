66b2a13fc5ea53eb25996d95606c478833765011
Punnoose Punnen <punnoose88@gmail.com>
2023-12-15 19:32:57 +0530

    Complete

--- app/(main)/_components/Publish.tsx
+++ app/(main)/_components/Publish.tsx
@@ -12,7 +12,7 @@ import { api } from '@/convex/_generated/api';
 import { useState } from 'react';
 import { toast } from 'sonner';
 import { Button } from '@/components/ui/button';
-import { ClipboardIcon, GlobeIcon } from 'lucide-react';
+import { CheckIcon, ClipboardIcon, CopyIcon, GlobeIcon } from 'lucide-react';
 
 interface PublishProps {
   initialData: Doc<'documents'>;
@@ -66,16 +66,49 @@ export const Publish = ({ initialData }: PublishProps) => {
 
   return (
     <Popover>
-      <PopoverTrigger asChild>
+      <PopoverTrigger>
         <Button size="sm" variant="ghost">
-          Publish
+          Published
           {initialData.isPublished && (
-            <GlobeIcon className="w-4 h-4 text-sky-500" />
+            <GlobeIcon className="w-4 h-4 text-sky-500 ml-2" />
           )}
         </Button>
         <PopoverContent className="w-72" align="end" alignOffset={8} forceMount>
           {initialData.isPublished ? (
-            <div>Published</div>
+            <div className="space-y-4 ">
+              <div className="flex items-center gap-x-2">
+                <GlobeIcon className="text-sky-500 animate-pulse h-4 w-4 " />
+                <p className="text-xs font-medium text-sky-500">
+                  This note is live on web!
+                </p>
+              </div>
+              <div className="flex items-center">
+                <input
+                  value={url}
+                  className="flex-1 px-2 text-xs border rounded-l-md h-8 bg-muted truncate"
+                  disabled
+                />
+                <Button
+                  onClick={onCopy}
+                  disabled={copied}
+                  className="h-8 rounded-l-none"
+                >
+                  {copied ? (
+                    <CheckIcon className="w-4 h-4 " />
+                  ) : (
+                    <CopyIcon className="h-4 w-4" />
+                  )}
+                </Button>
+              </div>
+              <Button
+                onClick={onUnPublish}
+                size="sm"
+                className="w-full text-xs"
+                disabled={isSubmitting}
+              >
+                Un-publish
+              </Button>
+            </div>
           ) : (
             <div className="flex flex-col items-center justify-center">
               <GlobeIcon className="h-8 w-8 text-muted-foreground mb-2" />
